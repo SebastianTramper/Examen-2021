756211818d7fd1538733e1c6f10c8f9ea9ec7ff9
Sebastian  Tramper <83488@b5-450-59.ict.lab.locals>
2021-05-19 17:09:00 +0200

    succes pagina gemaakt

--- dashboard/success.php
+++ dashboard/success.php
@@ -1,6 +1,7 @@
 
-<?php require_once "../includes/head.php";  ?>
 <?php require_once "../config/config.php"; ?>
+<?php require_once "../includes/head.php";  ?>
+
 
 <h1>Bedankt voor het aanmelden!</h1>
 <p>Print uw aanmeld bewijs uit door op downloaden te klikken.</p>
